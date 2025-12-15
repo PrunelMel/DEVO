0980706fd7ca7de09c475462a90894de518cd542
Prunel Mel <seyive.awaya@gmail.com>
2025-12-15 12:34:53 +0100

    removing garbage code

--- src/app/articles/aboutus/page.tsx
+++ src/app/articles/aboutus/page.tsx
@@ -2,64 +2,58 @@ import { getArticlesBySlug } from "@/lib/Article";
 
 import { notFound } from 'next/navigation';
 
-export default function ArticlePage({ params }: { params: { slug: string } }) {
+export default function ArticlePage() {
     
-  const article = getArticlesBySlug(params?.slug)
+
+  return (
     
-    if (!article) return notFound()
-  
-    return (
-      
-      <div>
-        <div className="w-full h-screen bg-stone-100 text-gray-600 font-mono">
-  
-          <div className="h-10"></div>
-  
-          <header className="font-bold text-6xl text-center ">ABOUT US</header>
-  
-          <article className="mt-32 w-full border flex-col p-6 flex justify-center items-center">
-  
-            <section className="flex-col w-1/2 ">
-  
-              <h1 className="font-bold text-4xl text-center ">{article.data.title1}</h1>
-              
-              <p className="text-justify m-4 text-3xl">{article.data.section1}</p>
+    <div>
+      <div className="w-full h-screen bg-stone-100 text-gray-600 font-mono">
+
+        <div className="h-10"></div>
+
+        <header className="font-bold text-6xl text-center ">ABOUT US</header>
+
+        <article className="mt-32 w-full border flex-col p-6 flex justify-center items-center">
+
+          <section className="flex-col w-1/2 ">
+
+            <h1 className="font-bold text-4xl text-center "></h1>
             
-            </section>
-  
-            <section className="flex-col w-1/2">
-              
-              <h2 className="font-bold text-4xl text-center">{article.data.title2}</h2>
-  
-              <p className="text-justify m-4 text-3xl">{article.content}</p>
-  
-  
-            </section>
-  
-          </article>
-        </div>
-
-        <div className="w-full h-screen bg-stone-100 text-gray-600 font-mono">
-  
-          <article>
-  
-            <section>
-  
-              <h3 className="font-bold text-2xl text-center">{article.data.title3}</h3>
-  
-              <p></p>
-              
-            </section>
+            <p className="text-justify m-4 text-3xl"></p>
           
-          </article>
-         
-          {/* <main className="prose dark:prose-invert max-w-3xl mx-auto py-8">
-            <p className="text-sm text-gray-500">{article.data.date}</p>
-            <div>{article.content}</div>
-          </main> */}
-        </div>
+          </section>
+
+          <section className="flex-col w-1/2">
+            
+            <h2 className="font-bold text-4xl text-center"></h2>
+
+            <p className="text-justify m-4 text-3xl"></p>
 
 
+          </section>
+
+        </article>
       </div>
-    )
-  }
+
+      <div className="w-full h-screen bg-stone-100 text-gray-600 font-mono">
+
+        <article>
+
+          <section>
+
+            <h3 className="font-bold text-2xl text-center"></h3>
+
+            <p></p>
+            
+          </section>
+        
+        </article>
+        
+      
+      </div>
+
+
+    </div>
+  )
+}
