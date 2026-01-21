a1a9b40a67bc2607b08af8506bbcddbc7c4bc1d7
Prunel Mel <seyive.awaya@gmail.com>
2026-01-21 17:40:29 +0100

    styling sub but

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { ThemeProvider } from "next-themes";
 import ThemeToggle from "../components/ThemeToggle";
-import Button from "../components/Button"
+
 import "./globals.css"
 
 const geistSans = Geist({
