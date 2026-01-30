91fd1ece3296ddfabb12738359bb0e391cf49bdf
Prunel Mel <seyive.awaya@gmail.com>
2026-01-30 13:33:54 +0100

    setting articlePage structure

--- src/app/articles/page.tsx
+++ src/app/articles/page.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import {opendir} from 'node:fs/promises';
+import Article from "@/components/Article";
 
 
 var files = await opendir('./src/app/articles');
@@ -17,27 +18,24 @@ export function page () {
   return (
     <div className="w-full h-screen">
 
-        <div className="w-full h-1/2 flex items-center justify-center">
+      <div className="w-full h-full flex flex-col items-center space-y-6">
 
+        <div className="h-12"></div>
 
-
-          <div className="flex-col ">
-
-              <div className="h-1/12">LATEST ARTICLES</div>
-
-              <div className="h-8/12">
-                {articles.map((article) => <div className="">{article}</div>)}
-              </div>
-
-
-              <div className="h-16"></div>
-
-              <span className="text-2xl">Come back Later ...</span>
-          </div>
-
-            
+        <div className="text-4xl text-center">LATEST ARTICLES</div>
         
-        </div>
+        <div className="h-30 w-3/4 border border-white border-solid "></div>
+        <Article></Article>
+        {/* <div className="flex-col space-y-8 h-full w-3/4">
+
+
+
+          <span className="text-2xl">Come back Later ...</span>
+        </div> */}
+
+          
+      
+      </div>
         
     </div>
   )
---
+++ src/components/Article.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Article = () => {
+  return (
+    <div className="h-30 w-3/4 border border-white border-solid "></div>
+  )
+}
+
+export default Article
