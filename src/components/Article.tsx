480b5ffbfa8fffb8cccbbb36e5707401942645b1
Prunel Mel <seyive.awaya@gmail.com>
2026-02-01 22:20:45 +0100

    adding ArticleView inner structure

--- src/components/Article.tsx
+++ src/components/Article.tsx
@@ -2,7 +2,13 @@ import React from 'react'
 
 const Article = () => {
   return (
-    <div className="h-30 w-3/4 border border-white border-solid "></div>
+    <div className="h-40 w-3/5 border border-white border-solid flex p-8">
+      <div className="w-full h-full flex space-x-5 justify-between">
+        <div className="w-25 h-full border border-white rounded-lg"></div>
+        <div className="text-3xl flex flex-col space-y-5 w-3/4"><p>Advanced React</p> <span className="text-sm pl-7">from DEVO</span></div>
+        <div className="w-16 h-16 border border-white rounded-full self-center justify-self-end"></div>
+      </div>
+    </div>
   )
 }
 
