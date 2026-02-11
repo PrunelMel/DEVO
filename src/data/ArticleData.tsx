45698160c31e9793ffb32fe282673239448e7de4
Prunel Mel <seyive.awaya@gmail.com>
2026-02-11 21:13:16 +0100

    adding ArticleData

---
+++ src/data/ArticleData.tsx
@@ -0,0 +1,16 @@
+interface ArticleData{
+    title:string,
+    source:string,
+    sourceImg:string,
+    level:string
+}
+
+const articles:ArticleData[] = [
+
+    {
+        title:"Balanced Brackets",
+        source:"HackerRank",
+        sourceImg:"/hackerRank/hackerRank.jpg",
+        level:"Hard"
+    }
+]
